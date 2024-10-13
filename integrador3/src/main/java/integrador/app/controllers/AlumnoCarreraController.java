7333d11fea4961b0c883a434bcb707caaf36b8de
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-10-13 20:34:33 -0300

    cambio en el metodo del controller para que ande bien

--- integrador3/src/main/java/integrador/app/controllers/AlumnoCarreraController.java
+++ integrador3/src/main/java/integrador/app/controllers/AlumnoCarreraController.java
@@ -12,10 +12,7 @@ import integrador.app.services.CarreraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -33,10 +30,19 @@ public class AlumnoCarreraController {
     private CarreraService cs;
 
     @GetMapping()
-    public ResponseEntity<?> getAll(@RequestParam(required = false ) String ciudad, @RequestParam(required = false ) String carrera) {
+    public ResponseEntity<?> getAll(/*@RequestParam(required = false ) String ciudad, @RequestParam(required = false ) String carrera*/) {
         try {
-            if (ciudad != null && carrera != null) ResponseEntity.status(HttpStatus.OK).body(alumnoCarreraService.getAlumnosByMajor(ciudad, carrera));
+        //  if (ciudad != null && carrera != null) ResponseEntity.status(HttpStatus.OK).body(alumnoCarreraService.getAlumnosByMajor(ciudad, carrera));
             return ResponseEntity.status(HttpStatus.OK).body(alumnoCarreraService.findAll());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/carrera/{carrera}/ciudad/{ciudad}")
+    public ResponseEntity<?> getByMajorAndCity(@PathVariable(name = "ciudad") String ciudad, @PathVariable(name = "carrera") String carrera){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(alumnoCarreraService.getAlumnosByMajor(ciudad, carrera));
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
