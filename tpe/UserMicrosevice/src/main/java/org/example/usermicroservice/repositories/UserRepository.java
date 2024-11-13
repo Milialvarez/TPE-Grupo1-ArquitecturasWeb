b955199b0110c0faa7fc5631419e261659ce8920
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-11-13 19:42:08 -0300

    consulta de getUsersByRole en el user repo

--- tpe/UserMicrosevice/src/main/java/org/example/usermicroservice/repositories/UserRepository.java
+++ tpe/UserMicrosevice/src/main/java/org/example/usermicroservice/repositories/UserRepository.java
@@ -1,9 +1,17 @@
 package org.example.usermicroservice.repositories;
 
+import org.example.usermicroservice.entities.Account;
 import org.example.usermicroservice.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.role.role = :rolName")
+    List<User> getUsersByRole(@Param("rolName") String rolName);
 }
