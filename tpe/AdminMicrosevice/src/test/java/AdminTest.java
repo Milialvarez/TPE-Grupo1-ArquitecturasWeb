7488658907006cbd53ea35d2f3edd589f30d3695
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-11-20 17:17:31 -0300

    agrego test a admin MS

--- tpe/AdminMicrosevice/src/test/java/AdminTest.java
+++ tpe/AdminMicrosevice/src/test/java/AdminTest.java
@@ -1,26 +1,51 @@
 import jdk.jfr.Description;
 import org.example.adminmicroservice.controllers.AdminController;
 import org.example.adminmicroservice.dtos.BillDTO;
+import org.example.adminmicroservice.feignClients.UserFeignClient;
+import org.example.adminmicroservice.models.User;
+import org.example.adminmicroservice.services.AdminService;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class AdminTest {
-    private static AdminController adminController;
+    @InjectMocks
+    private AdminService adminService;
+    @Mock
+    private UserFeignClient userFeignClient;
 
-    @BeforeAll
-    public static void setUp(){
-        adminController = new AdminController();
+    ResponseEntity<List<User>> usersresp;
+
+    @BeforeEach
+    void setUp() {
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        usersresp = ResponseEntity.ok(users);
     }
 
     @Test
-    @Description("Verifica que el código de respuesta sea 201")
-    public void testBillException(){
-        ResponseEntity<?> response = adminController.setNewBill(new BillDTO());
-        String statusCode = response.getStatusCode().toString();
-        assertEquals("201", statusCode);
+    public void correctResponse(){
+        when(userFeignClient.getUsersByRole("admin")).thenReturn(usersresp);
+        ResponseEntity<?> response = adminService.getUsersByRole("admin");
+        assert response != null;
+        assertEquals(Objects.requireNonNull(response.getBody()).getClass(), ArrayList.class, "clase incorrecta");
     }
 }
