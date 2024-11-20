0433f6442001f867cc3e4c18a5a1b4910d76b14a
Facundo Bravo <bfacundo176@gmail.com>
2024-11-20 11:29:02 -0300

    Actualizo permisos para acceder a mantenimiento

--- tpe/Gateway/src/main/java/micro/example/gateway/security/AuthotityConstant.java
+++ tpe/Gateway/src/main/java/micro/example/gateway/security/AuthotityConstant.java
@@ -5,6 +5,6 @@ public final class AuthotityConstant {
     private AuthotityConstant() {}
 
     public static final String _ADMINISTRADOR = "admin";
-    public static final String _USUARIO = "USUARIO";
-    public static final String _MANTENIMIENTO = "MANTENIMIENTO";
+    public static final String _USUARIO = "usuario";
+    public static final String _MANTENIMIENTO = "mantenimiento";
 }
