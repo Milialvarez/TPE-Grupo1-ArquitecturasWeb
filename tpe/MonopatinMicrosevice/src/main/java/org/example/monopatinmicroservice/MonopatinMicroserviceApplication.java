fe4696842297b9e40539aa02690a882dbc4da35c
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-11-20 10:31:21 -0300

    commit before checkout

--- tpe/MonopatinMicrosevice/src/main/java/org/example/monopatinmicroservice/MonopatinMicroserviceApplication.java
+++ tpe/MonopatinMicrosevice/src/main/java/org/example/monopatinmicroservice/MonopatinMicroserviceApplication.java
@@ -12,17 +12,17 @@ import java.text.ParseException;
 @SpringBootApplication
 @EnableFeignClients
 public class MonopatinMicroserviceApplication {
-//	@Autowired
-//	private DataLoaderHelper dataLoaderHelper;
+	@Autowired
+	private DataLoaderHelper dataLoaderHelper;
 	public static void main(String[] args) {
 		SpringApplication.run(MonopatinMicroserviceApplication.class, args);
 	}
 
-//	@PostConstruct
-//	public void init() throws ParseException {
-//		dataLoaderHelper.loadParadas();
-//		dataLoaderHelper.loadMonopatines();
-//		dataLoaderHelper.loadViajes();
-//		dataLoaderHelper.loadPausas();
-//	}
+	@PostConstruct
+	public void init() throws ParseException {
+		dataLoaderHelper.loadParadas();
+		dataLoaderHelper.loadMonopatines();
+		dataLoaderHelper.loadViajes();
+		dataLoaderHelper.loadPausas();
+	}
 }
