f5bf301f17a2a026818eec53c1c9b3288d0b83d3
milagros alvarez <milagrosalvarez2604@gmail.com>
2024-11-20 21:28:46 -0300

    agrego test a billing ms

--- tpe/BillingMicrosevice/src/test/java/BillTest.java
+++ tpe/BillingMicrosevice/src/test/java/BillTest.java
@@ -1,31 +1,49 @@
-import org.example.billingmicroservice.controllers.BillController;
+
+import org.example.billingmicroservice.dtos.BillDTO;
 import org.example.billingmicroservice.entities.Bill;
-import org.junit.jupiter.api.BeforeAll;
+import org.example.billingmicroservice.repositories.BillRepository;
+import org.example.billingmicroservice.services.BillService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class BillTest {
-    private static BillController billController;
+    @InjectMocks
+    private BillService billService;
+    @Mock
+    private BillRepository billRepository;
 
-    @BeforeAll
-    public static void setUp() {
-        billController = new BillController();
+    private BillDTO b;
+    private Bill bb;
+
+    @BeforeEach
+    void setUp() {
+        b = new BillDTO(LocalDate.now(), 40F, 5F);
+        bb = new Bill(1L, b.getFechaInicioFacturacionNueva(), 40F, 5F);
     }
 
     @Test
-    public void testAddBill() {
-        Date fV = new Date();
-        float pf = 1500;
-        float pex = 500;
+    void TEST_SetNewBill() throws Exception {
+        when(billRepository.save(any(Bill.class))).thenReturn(bb);
+        Bill bill = billRepository.save(bb);
 
-        ResponseEntity<?> response = billController.setNewBill(fV, pf, pex);
-        String status = response.getStatusCode().toString();
+        when(billRepository.findById(bill.getId())).thenReturn(Optional.of(bill));
 
-        assertEquals("201", status, "error, se debió recibir el código 201");
+        Bill bill2 = billService.getBillById(bill.getId());
+
+        assertNotNull(bill2, "La cuenta no se anuló correctamente");
     }
+
+
 
 }
